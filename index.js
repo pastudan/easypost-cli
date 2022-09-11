6ffb4e6be9d26944152971e0b97b32a9cd3a0910
Dan Pastusek <dan@hexial.com>
2022-09-11 12:51:49 -0500

    add customs info

--- index.js
+++ index.js
@@ -6,49 +6,72 @@ import chalk from "chalk";
 import fs from "fs";
 import Easypost from "@easypost/api";
 import terminalLink from "terminal-link";
+import { homedir } from "os";
+import { join } from "path";
+import mkdirp from "mkdirp";
 
-// import { init, sections } from "./utils.js";
+const easypostDir = join(homedir(), ".easypost-cli");
+mkdirp.sync(easypostDir);
+const envFileName = "config";
+const envFilePath = join(easypostDir, envFileName);
 
-dotenv.config();
-console.log("EasyPost CLI v0.0.1");
-const apiKey = init(false);
+dotenv.config({ path: envFilePath });
+let mode;
+
+// define a new console logger
+const blankLine = () => console.log("");
+const chalkMode = (arg) => chalk[mode === "PROD" ? "red" : "green"](arg);
+const line = (title, ...args) => {
+  console.log();
+  console.log(chalkMode(`[${mode} MODE]`), chalk.bold(title));
+  console.log(chalk.bgGreen(...args));
+};
+
+import pkg from "./package.json" assert { type: "json" };
+console.log(`EasyPost CLI v${pkg.version}`);
+const apiKey = init(process.argv[2]);
 const api = new Easypost(apiKey);
 sections();
-// listShipments();
 
-export function init(defaultToProd) {
-  let modeInput;
-  if (defaultToProd) {
-    modeInput = "P";
-  } else {
-    console.log("\nAPI Mode: (T)est / (p)rod?");
+export function init(modeInput) {
+  modeInput = modeInput?.toUpperCase();
+  if (!modeInput) {
+    // blankLine();
+    console.log("Start EasyPost CLI in Test or Prod mode? [T/p]");
     modeInput = reader.prompt().trim().toUpperCase();
   }
-  let mode = "TEST";
-  if (modeInput === "P") {
+  mode = "TEST";
+  if (modeInput === "P" || modeInput === "PROD") {
     mode = "PROD";
-    console.log(chalk.red("Starting in PROD mode"));
-  } else {
-    console.log(chalk.green("Starting in TEST mode"));
   }
   let apiKey = process.env[`EASYPOST_${mode}_API_KEY`];
   if (!apiKey) {
-    console.log(
-      `What is your ${mode} API key? (NOTE: This will be saved to .env)`
-    );
+    blankLine();
+
+    console.log(`What is your EasyPost ${chalkMode(mode)} API key?`);
     apiKey = reader.prompt();
     let env = "";
     try {
-      env = fs.readFileSync(".env");
+      env = fs.readFileSync(envFilePath);
     } catch {}
-    fs.writeFileSync(".env", env + `EASYPOST_${mode}_API_KEY=${apiKey}\n`);
+    fs.writeFileSync(envFilePath, env + `EASYPOST_${mode}_API_KEY=${apiKey}\n`);
+    console.log(
+      `Easypost ${chalkMode(mode)} API Key saved to ${join(
+        "~",
+        ".easypost-cli",
+        envFileName
+      )}`
+    );
   }
   return apiKey;
 }
 
 export async function sections() {
-  console.log(
-    "\n[S]hipments | [N]ew Shipment | [A]ddresses | [P]arcels | [Q]uit"
+  line(
+    "Main Menu",
+    chalk.bgGreen(
+      "[S] Shipments\t[N] New Shipment\t\t\n[A] Addresses\t[P] Parcels\t[Q] Quit\t"
+    )
   );
   const section = reader.prompt().trim().toUpperCase();
   if (section === "S") {
@@ -58,11 +81,11 @@ export async function sections() {
   } else if (section === "A") {
     await listAddresses();
   } else if (section === "P") {
-    await listParcels();
+    line("Parcels are not yet implemented");
   } else if (section === "Q") {
     process.exit(0);
   } else {
-    console.log(chalk.red("Invalid section"));
+    line(chalk.red("Invalid section"));
   }
   sections();
 }
@@ -127,6 +150,7 @@ const mapRate = (r) => {
     service: r.service,
     rate: r.rate,
     days: r.delivery_days,
+    id: r.id,
   };
 };
 
@@ -136,14 +160,17 @@ export async function listShipments() {
     purchased: false,
   });
   if (res.shipments.length === 0) {
-    console.log(chalk.red("No shipments found"));
+    line(chalk.red("No shipments found"));
     return sections();
   }
   const sortedShipments = res.shipments.sort(sortShipments);
-  // console.log(sortedShipments[0]);
+  // line(sortedShipments[0]);
   console.table(sortedShipments.map(mapShipment));
 
-  console.log("\n[#] Details | [Enter] Main Menu");
+  line(
+    `Showing first 10 Shipments (from last 30 days)`,
+    "[0-9] Details | [Enter] Main Menu"
+  );
 
   let shipment;
   while (!shipment) {
@@ -153,29 +180,72 @@ export async function listShipments() {
       return;
     }
     if (shipment) break;
-    console.log(chalk.red("Invalid selection"));
+    line(chalk.red("Invalid selection"));
   }
-  console.log("FROM");
-  console.table(mapAddress(shipment.from_address));
-  console.log("TO");
-  console.table(mapAddress(shipment.to_address));
-  console.log("PARCEL");
+  console.log(chalk.bold("From / To:"));
+  const keys = Object.keys(mapAddress(shipment.from_address));
+  const longestKey = keys.reduce((a, b) => (a.length > b.length ? a : b));
+  const longestFromKey = Object.values(mapAddress(shipment.from_address))
+    .map(toString)
+    .reduce((a, b) => (a.length > b.length ? a : b));
+  const longestToKey = Object.values(mapAddress(shipment.to_address))
+    .map(toString)
+    .reduce((a, b) => (a.length > b.length ? a : b));
+  console.table(
+    keys.map((key) => {
+      return {
+        field: `${key.toUpperCase()}: `.padStart(longestKey.length + 3),
+        From:
+          shipment.from_address[key]
+            ?.toString()
+            .padEnd(longestFromKey.length) || null,
+        To:
+          shipment.to_address[key]?.toString().padEnd(longestToKey.length) ||
+          null,
+      };
+    })
+  );
+  // console.table(mapAddress(shipment.from_address));
+  // console.log(chalk.bold("To:"));
+  // console.table(mapAddress(shipment.to_address));
+  console.log(chalk.bold("Parcel:"));
   console.table(mapParcel(shipment.parcel));
 
   if (shipment.selected_rate) {
-    console.log("RATE (showing selected rate only)");
+    console.log(chalk.bold("Selected Rate:"));
     console.table(mapRate(shipment.selected_rate));
   } else {
-    console.log("RATES");
+    console.log(chalk.bold("Available Rates:"));
     console.table(shipment.rates.sort(sortRates).map(mapRate));
   }
 
   shipment.selected_rate &&
-    console.log("Carrier:", shipment.selected_rate.carrier);
+    console.log(chalk.bold("Carrier:"), shipment.selected_rate.carrier);
   shipment.tracker &&
-    console.log(terminalLink("Track", shipment.tracker.public_url));
+    console.log(chalk.bold("Tracking:"), shipment.tracker.public_url);
   shipment.postage_label &&
-    console.log(terminalLink("Label URL", shipment.postage_label.label_url));
+    console.log(chalk.bold("Label URL:"), shipment.postage_label.label_url);
+
+  if (!shipment.selected_rate && shipment.rates.length > 0) {
+    line(
+      `Shipment has not been purchased. Purchase a rate? [0-9] Purchase rate | [Q] Main Menu`
+    );
+    const shipmentInput = reader.prompt().trim();
+    if (shipmentInput.toUpperCase() === "Q") {
+      return sections();
+    }
+    const rate = shipment.rates.sort(sortRates)[parseInt(shipmentInput, 10)];
+    if (!shipmentInput || !rate) {
+      line(chalk.red("Invalid selection"));
+      return sections();
+    }
+    await shipment.buy(rate.id);
+    line(
+      chalk.green(
+        `Rate Purchased, $${rate.rate} deducted from EasyPost balance.`
+      )
+    );
+  }
 }
 
 export async function newAddress() {
@@ -192,22 +262,39 @@ export async function newAddress() {
 }
 
 export async function newShipment() {
-  console.log("\nSelect or enter a FROM address:");
+  blankLine();
   const fromAddresses = await listAddresses();
-  console.log("[#] Selection | [N]ew Address | [Q]uit to main Menu");
+  line(
+    "Select or enter a FROM address",
+    "[0-9] Selection | [N] New Address | [Q] Quit to main Menu"
+  );
   const fromInput = reader.prompt().trim().toUpperCase();
   if (fromInput === "Q") return;
   const from_address =
     fromAddresses[parseInt(fromInput, 10)] || (await newAddress());
 
-  console.log("\nSelect or enter a TO address:");
+  blankLine();
   const toAddresses = await listAddresses();
-  console.log("[#] Selection | [N]ew Address | [Q]uit to main Menu");
+  line(
+    "Select or enter a TO address",
+    "[0-9] Selection | [N] New Address | [Q] Quit to main Menu"
+  );
   const toInput = reader.prompt().trim().toUpperCase();
   if (toInput === "Q") return;
   const to_address = toAddresses[parseInt(toInput, 10)] || (await newAddress());
 
-  console.log("\nPackage Dimensions");
+  let customs_info;
+  if (from_address.country !== to_address.country) {
+    line(
+      chalk.orange(
+        'Customs info required for international shipments. Please enter your "customs_info" ID:'
+      )
+    );
+    customs_info = reader.prompt().trim();
+  }
+
+  blankLine();
+  line("Package Dimensions");
   const parcel = await new api.Parcel({
     length: parseFloat(reader.question("Length (inch): ")),
     height: parseFloat(reader.question("Height (inch): ")),
@@ -219,41 +306,38 @@ export async function newShipment() {
     from_address,
     to_address,
     parcel,
+    customs_info,
   }).save();
 
-  console.log("\nSelect a rate to buy:");
+  blankLine();
+  line("Select a rate to buy:");
   console.table(shipment.rates.sort(sortRates).map(mapRate));
-  console.log("[#] Selection | [Q]uit to main menu");
+  line("[0-9] Selection | [Q]uit to main menu");
   let rate;
   while (!rate) {
     const rateInput = reader.prompt().trim();
     if (rateInput.toUpperCase() === "Q") return;
     rate = shipment.rates.sort(sortRates)[parseInt(rateInput, 10)];
     if (rateInput && rate) break;
-    console.log(chalk.red("Invalid selection - postage not purchased"));
+    line(chalk.red("Invalid selection - postage not purchased"));
   }
   await shipment.buy(rate.id);
-  console.log(
-    chalk.green(
-      `\nRate Purchased, $${rate.rate} deducted from EasyPost balance.`
-    )
+  blankLine();
+  line(
+    chalk.green(`Rate Purchased, $${rate.rate} deducted from EasyPost balance.`)
   );
-  console.log(terminalLink("Label URL", shipment.postage_label.label_url));
-
-  // console.log({ from_address, to_address, parcel });
+  line(terminalLink("Label URL", shipment.postage_label.label_url));
 }
 
 export async function listAddresses() {
   const res = await api.Address.all({ page_size: 5 });
-  // console.log(res.addresses[0]);
   if (res.addresses.length === 0) {
-    console.log(chalk.red("No addresses found"));
+    line(chalk.red("No addresses found"));
     return sections();
   }
   console.table(
     res.addresses.map((a) => {
       return {
-        // id: a.id,
         name: a.name,
         company: a.company,
         street1: a.street1,
